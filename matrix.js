30de82dc2f79f4cd3350d99e964b1f7dc61ff372
ffworker <162129412+ffworker@users.noreply.github.com>
2024-04-28 19:22:14 +0200

    Create matrix.js

---
+++ matrix.js
@@ -0,0 +1,17 @@
+var matrixCanvas = document.getElementById('matrixCanvas');
+matrixCanvas.height = window.innerHeight;
+matrixCanvas.width = window.innerWidth;
+var letters = Array(256).join(1).split('');
+
+function drawMatrix() {
+    matrixCanvas.getContext('2d').fillStyle='rgba(0,0,0,.05)';
+    matrixCanvas.getContext('2d').fillRect(0,0,matrixCanvas.width,matrixCanvas.height);
+    matrixCanvas.getContext('2d').fillStyle='#0F0';
+    letters.map(function(y_pos, index){
+        text = String.fromCharCode(3e4+Math.random()*33);
+        x_pos = index * 10;
+        matrixCanvas.getContext('2d').fillText(text, x_pos, y_pos);
+        letters[index] = (y_pos > 758 + Math.random() * 1e4) ? 0 : y_pos + 10;
+    });
+}
+setInterval(drawMatrix, 33);
